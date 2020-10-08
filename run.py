4ecc1ed66e3c0e8ae9d01d61e69347b6a7f47e6b
FedericoBlanc <flanc92@gmail.com>
2020-10-08 09:17:36 -0300

    Solved some imports

--- run.py
+++ run.py
@@ -1,13 +1,16 @@
-from defect_app import create_app,db
-app = create_app() # nothing passed because Config class is default
+from Defect_analyzer_front.defect_app import create_app, db
+
+app = create_app()  # nothing passed because Config class is default
+
+
 def init_db():
-	context = app.app_context()
-	context.push()
-	if 'users' not in db.get_engine(app=app,bind=None).table_names():
-		db.create_all()
+    context = app.app_context()
+    context.push()
+    if 'users' not in db.get_engine(app=app, bind=None).table_names():
+        db.create_all()
+
 
 if __name__ == '__main__':
+    init_db()
 
-	init_db()
-
-	app.run(host='0.0.0.0', port=5000)
+    app.run(host='0.0.0.0', port=5000)
