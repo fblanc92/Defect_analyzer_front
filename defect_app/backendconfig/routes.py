1edb1c36d9ed416cc705274edccf911581f8ff02
FedericoBlanc <flanc92@gmail.com>
2020-10-13 11:06:18 -0300

    Implemented: Revert config

--- defect_app/backendconfig/routes.py
+++ defect_app/backendconfig/routes.py
@@ -1,10 +1,10 @@
 from flask_login import current_user, login_required
 from flask import render_template, request, Blueprint, flash, redirect, url_for, abort
 from Defect_analyzer_front.defect_app import db
-from Defect_analyzer_front.defect_app.models import Backend_config
+from Defect_analyzer_front.defect_app.models import Backend_config, Coil_post
 from Defect_analyzer_front.defect_app.backendconfig.forms import BackendConfigForm
 from Defect_analyzer_back.resources.config.configs_utils import get_all_the_configs_list
-from Defect_analyzer_back.resources.config.configs_utils import update_config, get_current_config_json
+from Defect_analyzer_back.resources.config.configs_utils import update_config, get_current_config_json, revert_config
 
 backend_config_blueprint = Blueprint('backend_config_blueprint', __name__)
 
@@ -15,6 +15,14 @@ def backend_config():  # Home | About | Analyzer Configs | Configurations
     page = request.args.get('page', 1, type=int)
     backend_config_list = get_all_the_configs_list()
     # configs = Backend_config.query.order_by(Backend_config.date_created.desc()).paginate(page=page, per_page=5)
+    return render_template('backend_config.html', configs=backend_config_list)
+
+
+@backend_config_blueprint.route("/backend_config/revert")
+@login_required
+def revert_backend_config():
+    revert_config()
+    backend_config_list = get_all_the_configs_list()
     return render_template('backend_config.html', configs=backend_config_list)
 
 
@@ -51,7 +59,8 @@ def edit_backend_config():  # no argument config_id argument because the idea is
     form.scan_timer_delay.data = current_config_json['config']['scan_timer_delay']
     form.path_to_labels.data = current_config_json['config']['path_to_labels']
     form.path_to_frozen_graph.data = current_config_json['config']['path_to_frozen_graph']
-    form.path_to_current_coil_register_folder.data = current_config_json['config']['path_to_current_coil_register_folder']
+    form.path_to_current_coil_register_folder.data = current_config_json['config'][
+        'path_to_current_coil_register_folder']
     form.path_to_current_coil_register_json.data = current_config_json['config']['path_to_current_coil_register_json']
     form.path_to_output_folders.data = current_config_json['config']['path_to_output_folders']
     form.path_to_current_config_folder.data = current_config_json['config']['path_to_current_config_folder']
