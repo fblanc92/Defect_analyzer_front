558bf744597776a6631c728413c9a3a43a3bf3f6
FedericoBlanc <flanc92@gmail.com>
2020-10-13 09:14:20 -0300

    minor changes

--- run_frontend.py
+++ run_frontend.py
@@ -1,5 +1,5 @@
 from Defect_analyzer_front.defect_app import create_app, db
-import threading
+
 
 app = create_app()  # nothing given as arg because Config class is default
 
@@ -9,14 +9,8 @@ def init_db():
     context.push()
     if 'users' not in db.get_engine(app=app, bind=None).table_names():
         db.create_all()
+    return
 
 
 def start_frontend():
-    app_thread = threading.Thread(target=app.run(host='0.0.0.0', port=5000)).start()
-
-
-
-# if __name__ == '__main__':
-    # init_db()
-#     # start_backend()
-    # start_frontend()
+    app.run(host='0.0.0.0', port=5000)
